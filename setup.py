0ac50d692c0a63829bc9a3a0c85cf854f68922bb
Allan Pillai <pillaiallan4@gmail.com>
2024-11-07 16:20:47 +0100

    changes in setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        # Add any dependencies here, for example:
-        # 'numpy', 'requests'
     ],
     entry_points={
         'console_scripts': [
